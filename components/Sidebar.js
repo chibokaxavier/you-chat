b0caa236189c0169ed3a9cf2e8697288fb71d2fa
manlikexavy <chibokaxavier@gmail.com>
2023-05-24 07:39:59 -0700

    SEARCH BOX COMPLETED

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,13 @@
-import { Chat, MoreVert } from "@mui/icons-material";
+import { Chat, MoreVert, Search } from "@mui/icons-material";
 import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <div>
+      {/* HEADER */}
       <div className="flex sticky top-0 bg-white  justify-between z-10 items-center p-4 h-[80px] border-b border-[whitesmoke]">
-        <Avatar  className="cursor-pointer hover:opacity-[0.8]"/>
+        <Avatar className="cursor-pointer hover:opacity-[0.8]" />
         <div>
           <IconButton>
             {" "}
@@ -17,6 +18,11 @@ const Sidebar = () => {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+      {/* SEARCH */}
+      <div className="flex  items-center p-[5px] border-2 ">
+        <Search />
+        <input type="text" placeholder="Search in chats" className="outline border-none outline-none flex-1"/>
       </div>
     </div>
   );
